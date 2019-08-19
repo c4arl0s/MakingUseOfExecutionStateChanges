44de6159b7019a6eab4c9a7a6081da95bd57749c
c4arl0s <c.santiago.cruz@gmail.com>
2019-08-18 19:08:48 -0500

    Add project

---
+++ HandlingTheInactiveState/HandlingTheInactiveState/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  HandlingTheInactiveState
+//
+//  Created by Carlos Santiago Cruz on 8/17/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) UILabel *label;
+
+@end
+
